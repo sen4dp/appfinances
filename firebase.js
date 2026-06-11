24f77bd9bf94ab03500f9271c74c7eed4fcf164c
Dennys Perdomo <sena.dennys.perdomo@gmail.com>
2026-06-10 21:16:52 -0500

    CHANGES FIREBASE

--- firebase.js
+++ firebase.js
@@ -1,8 +1,11 @@
-// 1. Importas las funciones que necesitas desde la librería que instalaste con npm
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// 2. Las credenciales únicas de tu aplicación (las tuyas de la imagen)
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDMtwhJvSLmdfv0mUT5VkzS4aqY81A9_80",
   authDomain: "appfinances-e3f63.firebaseapp.com",
@@ -13,9 +16,6 @@ const firebaseConfig = {
   measurementId: "G-G5LGLZGG35"
 };
 
-// 3. Inicializas Firebase pasándole tu configuración
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-// 4. (Opcional pero recomendado) Exportas la app para usarla en tus otros archivos de JS
-export { app };
